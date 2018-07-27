60d4728d11e9901e41e7924dc1ac5397dcb98835
Thinh <phamduythinh258@gmail.com>
2018-07-27 09:10:51 +0700

    client login

--- ClientApp/Login.cs
+++ ClientApp/Login.cs
@@ -20,12 +20,21 @@ namespace ClientApp
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            ProcessStartInfo sInfo = new ProcessStartInfo("http://www.google.com");
+            ProcessStartInfo sInfo = new ProcessStartInfo("http://project.com");
             Process.Start(sInfo);
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                string message = "Username can't be blank";
+                if (string.IsNullOrEmpty(textBox2.Text))
+                {
+                    message += ", Password can't be blank";
+                }
+                MessageBox.Show(message, "Error",MessageBoxButtons.OK,MessageBoxIcon.Exclamation );
+            }
             HttpClient client = new HttpClient();
             var values = new Dictionary<string, string>
             {
@@ -35,7 +44,25 @@ namespace ClientApp
             var content = new FormUrlEncodedContent(values);
             var response = await client.PostAsync("http://localhost:52598/AppLogin.ashx", content);
             string s = await response.Content.ReadAsStringAsync();
-            label1.Text = s;
+            bool success = false;
+            try
+            {
+                success = bool.Parse(s);
+            }
+            catch
+            {
+                success = false;
+            }
+            if (!success)
+            {
+                label3.Visible = true;
+            }
+            else
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
         }
+
     }
 }
